1264f455dde3af7a9a0a7367d8a0b3b72b4abe1b
Julian Fleck <mail@julianfleck.net>
2024-11-12 20:39:28 +0100

    feat(auth): registered auth blueprint

--- app/api/v1/__init__.py
+++ app/api/v1/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
 from app.api.v1.endpoints.health import health_bp
+from app.api.v1.endpoints.auth import auth_bp
 
 api_v1 = Blueprint('api_v1', __name__, url_prefix='/v1')
 api_v1.register_blueprint(health_bp)
+api_v1.register_blueprint(auth_bp, url_prefix='/auth')
